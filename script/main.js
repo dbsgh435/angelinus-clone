7d57f3cfc3067498e5117b5cbea385ababae317c
dbsgh435 <84379680+dbsgh435@users.noreply.github.com>
2021-10-29 19:10:34 +0900

    슬라이드 오류 해결 진행중

--- script/main.js
+++ script/main.js
@@ -1,7 +1,20 @@
-setInterval(function() {
-  $(".slide").animate({
-    left : "-=100%"
-  }, function() {
-    $(".slide").children().first().appendTo(this)
+$(function() {
+  setInterval(function() {
+    $(".slide").animate({
+      left : "-=100%"
+    }, function() {
+      $(".slide").children().first().appendTo(this)
+    })
+  },3000)
+    
+
+  $(".gnb").mouseover(function() {
+    $(".lnb").stop().slideDown(),
+    $(".lnb-bg").stop().removeClass("blind"),
+    $(".lnb-bg").stop().show()
   })
-},3000)
+  $(".gnb").mouseout(function() {
+    $(".lnb").stop().hide(),
+    $(".lnb-bg").stop().addClass("blind")
+  })
+})
